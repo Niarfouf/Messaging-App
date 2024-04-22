90961d863f30bfa62069a803a5b50d4749397c61
Niarfouf <tniarfeix@gmail.com>
2024-04-22 11:28:06 +0200

    Add: skeleton express app

---
+++ back-end/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const createError = require("http-errors");
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // return error
+  res.status(err.status || 500);
+  res.json({ message: err.message });
+});
+
+module.exports = app;
